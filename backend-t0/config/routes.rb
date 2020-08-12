17e2f5061cb799e9e9ffac0731e60fb227e20e0d
Jandresgomez <jagv1998@gmail.com>
2020-08-11 21:32:06 -0500

    Working auth

--- backend-t0/config/routes.rb
+++ backend-t0/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    post '/api-auth', to: 'authentication#authenticate'
+    post '/create-user', to: 'user#signup'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
